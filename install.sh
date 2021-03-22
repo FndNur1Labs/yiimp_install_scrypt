c113eac4cbd1642bba7dfe1b2a5cc6a3ebc0fab3
Founder Nur1Labs <FndNur1Labs@users.noreply.github.com>
2021-03-22 22:47:44 +0700

    update Kudaraidee

--- install.sh
+++ install.sh
@@ -180,7 +180,7 @@
     apt_install php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli \
     php7.3-cgi php-pear imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt\
     php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick php-gettext php7.3-zip php7.3-mbstring \
-    libpsl-dev libnghttp2-dev
+    libpsl-dev libnghttp2-dev php7.3-memcache php7.3-memcached
     fi
     sleep 5
     hide_output sudo systemctl start php7.3-fpm
@@ -369,7 +369,7 @@
     
     # Compil Blocknotify
     cd ~
-    hide_output git clone https://github.com/tpruvot/yiimp
+    hide_output git clone https://github.com/Kudaraidee/yiimp
     cd $HOME/yiimp/blocknotify
     sudo sed -i 's/tu8tu5/'$blckntifypass'/' blocknotify.cpp
     hide_output sudo make
